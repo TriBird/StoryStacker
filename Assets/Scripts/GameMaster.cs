1b3a1af4b226def0793a6d720eebf3d6e170ef0c
TriBrid <shigesaku.390@gmail.com>
2023-11-04 02:09:54 +0900

    First animation

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,10 @@ using DG.Tweening;
 
 public class GameMaster : MonoBehaviour{
 
-	public Transform StoryBoardTop_Trans, StoryBoard_Trans, Stacker_Trans;
+	public Transform StoryBoardTop_Trans, StoryBoard_Trans, Stacker_Trans, Floor_Trans;
+	public GameObject StoryBrave_Prefab;
 
+	private List<StoryCtrl> stackedstories = new List<StoryCtrl>();
 	private int currentstack;
 
 	void Start(){
@@ -22,5 +25,31 @@ public class GameMaster : MonoBehaviour{
 		story_instance.transform.DOLocalMove(new Vector3(Random.Range(-35f, 35f), -390+(50*currentstack), 0), 0.2f);
 
 		currentstack += 1;
+		stackedstories.Add(story_instance);
+	}
+
+	// take out story
+	public void TakeoutStory(){
+		StoryCtrl story = stackedstories[stackedstories.Count - 1];
+		stackedstories.RemoveAt(stackedstories.Count - 1);
+
+		story.transform.DOLocalMove(new Vector3(700,280,0), 0.5f);
+	}
+
+	// Press story play button
+	public void PlayStory(){
+		StartCoroutine(StoryPlayInit());
+	}
+	public IEnumerator StoryPlayInit(){
+		GameObject obj = Instantiate(StoryBrave_Prefab, Stacker_Trans);
+		obj.transform.localPosition = new Vector3(0f, 600f, 0f);
+		StoryStacking(obj.transform.GetComponent<StoryCtrl>());
+		yield return new WaitForSeconds(0.5f);
+
+		Floor_Trans.transform.DOLocalMoveX(0, 0.5f);
+		yield return new WaitForSeconds(0.5f);
+
+		// Take out the stories one by one 
+		TakeoutStory();
 	}
 }
